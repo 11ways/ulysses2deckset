b5528351f67172853380d06621974b1ad6d6a3ff
Jelle De Loecker <jelle@develry.be>
2018-02-26 19:26:25 +0100

    Fix theme settings being pasted on the end instead of the top

--- index.js
+++ index.js
@@ -102,10 +102,11 @@ generateDeck = Blast.Bound.Function.throttle(function generateDeck() {
 		results = Blast.Bound.Array.flatten(results).join('\n\n---\n\n');
 
 		// Add theme settings to the top
-		results += 'footer: Introduction to a11y & Inclusive Design • by Roel Van Gils (roel@11ways.be)\n'
+		results = 'footer: Introduction to a11y & Inclusive Design • by Roel Van Gils (roel@11ways.be)\n'
 		        +  'slidenumbers: true\n'
 		        +  'autoscale: true\n'
-		        +  'theme: Work, 3\n\n';
+		        +  'theme: Work, 3\n\n'
+		        + results;
 
 		// Count the slides
 		slides_count = 1 + Blast.Bound.String.count(results, '\n---\n');
